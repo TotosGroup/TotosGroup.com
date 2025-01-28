2d4edf83046bea947c6f449d65bc3abe04763924
TotosGroup <163785683+TotosGroup@users.noreply.github.com>
2025-01-28 02:42:27 +0200

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,176 @@
+<section id="footer" class="pt-5">
+	<div class="container">
+		<div class="row footer_1">
+			<div class="col-md-3">
+				<div class="footer_1i">
+					<h3 class="mb-4"><a class="text-white" href="index.php"> <span class="col_oran">Totos</span>
+							Group</a></h3>
+					<p class="col_light font_15 mb-4">Totos Group delivers end-to-end solutions, combining retail,
+						travel, and mobility to simplify and enhance everyday life and exploration.<br> Contact us for
+						online appointment now.</p>
+					<ul class="social-network social-circle mb-0 mt-2">
+						<li><a href="https://wa.me/201014157552?text=Hi%20Totos%20Group" class="icoWhatsapp"
+								title="WhatsApp"><i class="fa fa-whatsapp"></i></a></li>
+						<li><a href="https://www.facebook.com/Totos.Group" class="icoFacebook" title="Facebook"><i
+									class="fa fa-facebook"></i></a></li>
+						<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="footer_1i">
+					<h4 class="mb-4 text-white">News</h4>
+					<div class="footer_1i1 row">
+						<div class="col-md-3 col-3">
+							<div class="footer_1i1l">
+								<img src="img/15.jpg" alt="abc" class="rounded-circle w-100">
+							</div>
+						</div>
+						<div class="col-md-9 px-0 col-9">
+							<div class="footer_1i1r">
+								<p class="font_15 mb-0"><a class="col_light" href="#">Totos Amazon Store Reaches 2,000
+										Products Milestone!</a></p>
+								<p class="col_oran font_13 fw-bold">Aug 1, 2022</p>
+							</div>
+						</div>
+					</div>
+					<div class="footer_1i1 row">
+						<div class="col-md-3 col-3">
+							<div class="footer_1i1l">
+								<img src="img/16.jpg" alt="abc" class="rounded-circle w-100">
+							</div>
+						</div>
+						<div class="col-md-9 px-0 col-9">
+							<div class="footer_1i1r">
+								<p class="font_15 mb-0"><a class="col_light" href="#">Alexandria Information Center
+										Launches New Interactive Maps</a></p>
+								<p class="col_oran font_13 fw-bold">Sep 23, 2021</p>
+							</div>
+						</div>
+					</div>
+					<div class="footer_1i1 row">
+						<div class="col-md-3 col-3">
+							<div class="footer_1i1l">
+								<img src="img/17.jpg" alt="abc" class="rounded-circle w-100">
+							</div>
+						</div>
+						<div class="col-md-9 px-0 col-9">
+							<div class="footer_1i1r">
+								<p class="font_15 mb-0"><a class="col_light" href="#">Totos CarGo Expands Services to
+										Siwa Oasis</a></p>
+								<p class="col_oran font_13 fw-bold">May 13, 2022</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="footer_1i2">
+					<h4 class="mb-4 text-white">Tags</h4>
+					<ul>
+						<li class="d-inline-block"><a href="#">Alexandria</a></li>
+						<li class="d-inline-block"><a href="#">Amazon</a></li>
+						<li class="d-inline-block"><a href="#">Siwa</a></li>
+						<li class="d-inline-block"><a href="#">Siwa Guide</a></li>
+						<li class="d-inline-block"><a href="#">Hospitality</a></li>
+						<li class="d-inline-block"><a href="#">Lifestyle</a></li>
+						<li class="d-inline-block"><a href="#">E-Commerce</a></li>
+						<li class="d-inline-block"><a href="#">Tourism</a></li>
+
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="footer_1i2">
+					<h4 class="mb-4 text-white">Gallery</h4>
+					<div class="footer_1i2i row">
+						<div class="col-md-4 col-4 px-1">
+							<div class="footer_1i2i1">
+								<a href="#"><img src="img/28.jpg" class="w-100" alt="abc"></a>
+							</div>
+						</div>
+						<div class="col-md-4 col-4 px-1">
+							<div class="footer_1i2i1">
+								<a href="#"><img src="img/29.jpg" class="w-100" alt="abc"></a>
+							</div>
+						</div>
+						<div class="col-md-4 col-4 px-1">
+							<div class="footer_1i2i1">
+								<a href="#"><img src="img/30.jpg" class="w-100" alt="abc"></a>
+							</div>
+						</div>
+					</div>
+					<div class="footer_1i2i row mt-2">
+						<div class="col-md-4 col-4 px-1">
+							<div class="footer_1i2i1">
+								<a href="#"><img src="img/31.jpg" class="w-100" alt="abc"></a>
+							</div>
+						</div>
+						<div class="col-md-4 col-4 px-1">
+							<div class="footer_1i2i1">
+								<a href="#"><img src="img/32.jpg" class="w-100" alt="abc"></a>
+							</div>
+						</div>
+						<div class="col-md-4 col-4 px-1">
+							<div class="footer_1i2i1">
+								<a href="#"><img src="img/33.jpg" class="w-100" alt="abc"></a>
+							</div>
+						</div>
+					</div>
+					<div class="footer_1i2i row mt-2">
+						<div class="col-md-4 col-4 px-1">
+							<div class="footer_1i2i1">
+								<a href="#"><img src="img/34.jpg" class="w-100" alt="abc"></a>
+							</div>
+						</div>
+						<div class="col-md-4 col-4 px-1">
+							<div class="footer_1i2i1">
+								<a href="#"><img src="img/35.jpg" class="w-100" alt="abc"></a>
+							</div>
+						</div>
+						<div class="col-md-4 col-4 px-1">
+							<div class="footer_1i2i1">
+								<a href="#"><img src="img/36.jpg" class="w-100" alt="abc"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="footer_b" class="pt-3 pb-3">
+	<div class="container">
+		<div class="row footer_b_1 text-center">
+			<div class="col-md-12">
+				<p class="mb-0 col_light font_15">© 2025 <a class="col_oran"
+						href="https://www.facebook.com/Totos.Group">Totos Group</a> All Rights Reserved | Design by <a
+						class="col_oran" href="https://www.facebook.com/Mohamed.Aly.Othman">Mohamed Othman</a></p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<script>
+	window.onscroll = function () { myFunction() };
+
+	var navbar_sticky = document.getElementById("navbar_sticky");
+	var sticky = navbar_sticky.offsetTop;
+	var navbar_height = document.querySelector('.navbar').offsetHeight;
+
+	function myFunction() {
+		if (window.pageYOffset >= sticky + navbar_height) {
+			navbar_sticky.classList.add("sticky")
+			document.body.style.paddingTop = navbar_height + 'px';
+		} else {
+			navbar_sticky.classList.remove("sticky");
+			document.body.style.paddingTop = '0'
+		}
+	}
+</script>
+
+</body>
+
+</html>
---
+++ inc/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/global.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+	<link rel="icon" href="img/logo.png" type="image/x-icon">
+
+
+	<script src="js/bootstrap.bundle.min.js"></script>
